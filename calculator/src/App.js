7cd083e3b2ce8be17e3bcc7998860be437bfd1a1
Amruthavalli Chivukula <115539563+CamruthaV@users.noreply.github.com>
2024-04-30 08:56:19 +0530

    Add files via upload

---
+++ calculator/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Calculator from './calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Simple Calculator</h1>
+        <Calculator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
