bdd28d2b6d3cef7b20cc1fc33783ea9a3d24e75d
Amruthavalli Chivukula <115539563+CamruthaV@users.noreply.github.com>
2024-04-30 09:13:37 +0530

    Update calculator.js

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -13,7 +13,9 @@ const Calculator = () => {
 
   const calculate = () => {
     try {
-      setResult(eval(result).toString());
+      const expression = result;
+      const evalFn = new Function('return ' + expression);
+      setResult(evalFn().toString());
     } catch (error) {
       setResult('Error');
     }
@@ -21,22 +23,22 @@ const Calculator = () => {
 
   return (
     <div className="calculator">
-      <input type="text" value={result} />
+      <input type="text" value={result} readOnly />
       <div className="keypad">
-        <button onClick={clear} id="clear">Clear</button><br>
+        <button onClick={clear} id="clear">Clear</button>
         <button name="/" onClick={handleClick}>/</button>
         <button name="7" onClick={handleClick}>7</button>
         <button name="8" onClick={handleClick}>8</button>
         <button name="9" onClick={handleClick}>9</button>
-        <button name="*" onClick={handleClick}>*</button><br>
+        <button name="*" onClick={handleClick}>*</button>
         <button name="4" onClick={handleClick}>4</button>
         <button name="5" onClick={handleClick}>5</button>
-        <button name="6" onClick={handleClick}>6</button><br>
+        <button name="6" onClick={handleClick}>6</button>
         <button name="-" onClick={handleClick}>-</button>
         <button name="1" onClick={handleClick}>1</button>
         <button name="2" onClick={handleClick}>2</button>
         <button name="3" onClick={handleClick}>3</button>
-        <button name="+" onClick={handleClick}>+</button><br>
+        <button name="+" onClick={handleClick}>+</button>
         <button name="0" onClick={handleClick}>0</button>
         <button name="." onClick={handleClick}>.</button>
         <button onClick={calculate} id="equals">=</button>
